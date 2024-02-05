8f29ca7199ce0c9c573ee9c0b0a976d0ff47ff00
ahguney <ahmetguney090@gmail.com>
2024-02-05 16:35:06 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,35 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Id = 1;
+        person.FirstName = "AHMET FAHRİ";
+        person.LastName = "GÜNEY";
+        person.DateofBirthYear = 1;
+        person.NationalIdentity = 1234567890;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person);
+
+    }
+    private static void Variables()
+    {
+        string message = "Hello";
+        double price = 100000;
+        int number = 100;
+        bool isAuthenticate = false;
+
+        string firstName = "Ahmet Fahri";
+        string lastName = "Güney";
+        long nationalIdentity = 1234567890;
+
+        Console.WriteLine(price * 1.18);
+
+        Console.WriteLine(price * 1.18);
+    }
+}
